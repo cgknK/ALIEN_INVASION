503123838c1f99aeb6be5222ddc7f1e9946710b6
cKey <klvzcgkn@gmail.com>
2023-05-12 02:07:48 +0300

    Settings sınıfı oluşturuldu ve drivera entegre edildi

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """
     Overall class to manage game assets and behavior.
@@ -10,14 +12,12 @@ class AlienInvasion:
         """
         Initialize the game, and create game resources.
         """
-
         pygame.init()
+        self.settings = Settings()
 
-        # neden set_mode iki parantezler yazılmış?
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -32,7 +32,7 @@ class AlienInvasion:
             # Redraw the screen during each pass through the loop.
             #bu fill() methodu nereden geliyor?(pygame.display mı?)
             #sanki pygame.display.fill() olmalıymış gibi geliyor mantığı düzelt
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
