d6aeacc44f9b23cba46d435124c16b0ac54c7334
cKey <klvzcgkn@gmail.com>
2023-05-12 01:51:32 +0300

    Arka plan rengi triplet 230 yapıldı

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,6 +16,8 @@ class AlienInvasion:
         # neden set_mode iki parantezler yazılmış?
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -27,6 +29,10 @@ class AlienInvasion:
                     # bu exit() c/c++ daki exit()-abort()- gibi mi?
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop.
+            #bu fill() methodu nereden geliyor?(pygame.display mı?)
+            #sanki pygame.display.fill() olmalıymış gibi geliyor mantığı düzelt
+            self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
