64bcfeb45b5009d470ff12a7b588ff18a1fed4eb
cKey <klvzcgkn@gmail.com>
2023-05-19 15:19:43 +0300

    Filonun sağa hareket ederek kayboluyor

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,6 +43,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -114,6 +115,10 @@ class AlienInvasion:
         _Neden terminale birşey yazdırmak daha çok zaman alıyor?
         """
         #print(len(self.bullets))
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
